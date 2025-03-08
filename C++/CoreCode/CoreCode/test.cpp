5533983aa1a2532feb1ba8976c40905bf5c0471b
webliert <81344809+webliert@users.noreply.github.com>
2025-03-08 21:38:16 +0800

    经过测试，发现之所以无法编译和调试通过的原因是生成文件为中文时无法找到。

---
+++ C++/CoreCode/CoreCode/test.cpp
@@ -0,0 +1,24 @@
+/**
+ * @FilePath     : /C-program/C++/CoreCode/CoreCode/test.cpp
+ * @Description  :  测试引用的基本概念，注意事项等
+ * @Author       : webliert 81344809+webliert@users.noreply.github.com
+ * @Version      : 0.0.1
+ * @LastEditors  : webliert 81344809+webliert@users.noreply.github.com
+ * @LastEditTime : 2025-03-08 21:37:00
+ * @Copyright    : G AUTOMOBILE RESEARCH INSTITUTE CO.,LTD Copyright (c) 2025.
+**/
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+//引用的基本概念
+int main(int argc, const char** argv) {
+    int a = 10;
+    int &b = a;
+    cout << "a的地址为" << &a << endl;
+    cout << "b的地址为" << &b << endl;
+    cout << "a的值为" << a << endl; 
+    cout << "b的值为" << b << endl;
+    return 0;
+}
